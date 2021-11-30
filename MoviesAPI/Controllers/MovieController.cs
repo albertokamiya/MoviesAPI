99df30db257c29823a13ab3058f9ed901cee1d93
Alberto Kamiya <alberto.kamiya@gmail.com>
2021-11-30 17:58:16 -0300

    commit inicial

---
+++ MoviesAPI/Controllers/MovieController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Models;
+
+namespace MoviesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        private static List<Movie> _movies = new List<Movie>();
+
+        [HttpPost]
+        public void AddMovie([FromBody] Movie movie)
+        {
+            _movies.Add(movie);
+            Console.WriteLine(movie.Title);
+        }
+    }
+}
---
+++ MoviesAPI/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace MoviesAPI.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        public int Duration { get; set; }
+    }
+}
