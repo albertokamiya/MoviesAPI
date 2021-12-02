70cee75ede3210522b25a8da18c3926e64c23557
Alberto Kamiya <alberto.kamiya@gmail.com>
2021-12-02 11:49:48 -0300

    retorno dos dados

--- MoviesAPI/Controllers/MovieController.cs
+++ MoviesAPI/Controllers/MovieController.cs
@@ -9,12 +9,31 @@ namespace MoviesAPI.Controllers
     public class MovieController : ControllerBase
     {
         private static List<Movie> _movies = new List<Movie>();
+        private static int _count = 0;
 
         [HttpPost]
-        public void AddMovie([FromBody] Movie movie)
+        public IActionResult AddMovie([FromBody] Movie movie)
         {
+            movie.Id = ++_count;
             _movies.Add(movie);
-            Console.WriteLine(movie.Title);
+            return CreatedAtAction(nameof(GetMovieById), new { Id = movie.Id }, movie);
+        }
+
+        [HttpGet]
+        public IActionResult listMovies()
+        {
+            return Ok(_movies);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetMovieById(int id)
+        {
+            Movie movie = _movies.FirstOrDefault(movie => movie.Id == id);
+            if (movie != null)
+            {
+                return Ok(movie);
+            }
+            return NotFound();
         }
     }
 }
